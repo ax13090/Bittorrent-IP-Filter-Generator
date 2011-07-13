ba2da223eaea63a70ca087df10ba09d71933e1a0
Axel de Sablet <axel@macbook.local>
2011-07-13 22:43:26 +0200

    Suppression de code inutile (le cas else etait du code mort)

--- charger_listes.py
+++ charger_listes.py
@@ -7,23 +7,16 @@ chemin_bt_level_1 = r'http://list.iblocklist.com/?list=bt_level1&fileformat=p2p&
 chemin_bt_level_3 = r'http://list.iblocklist.com/?list=bt_level3&fileformat=p2p&archiveformat=gz'
 
 def charger(chemin):
-
 	request = Request(chemin)
 	request.add_header('Accept-encoding', 'gzip')
 	response = urlopen(request)
 	print(response.info())
 	print('Content-Encoding', response.info().get('Content-Encoding') )
 	info = response.info()
-	if True or info.get('Content-Encoding') == 'gzip' or info.get('Content-Type') == 'application/x-gzip':		
-		print('if')
-		buf = BytesIO( response.read())
-		f = gzip.GzipFile(fileobj=buf)
-		data = f.read()
-		print(data)
-	else:
-		print('else')
-		print(response.read())
-	pass;
+	buf = BytesIO( response.read())
+	f = gzip.GzipFile(fileobj=buf)
+	data = f.read()
+	print(data)
 
 
 if __name__ == '__main__':
