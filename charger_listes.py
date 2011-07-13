074ff2204f8da91dc0ec519f63de854a416f7a9e
Axel <axel@macbook.local>
2011-07-13 22:41:00 +0200

    Utilisation de BytesIO, GZip, urllib.request.Request. La recuperation du texte d'un fichier de liste d'adresses IO a filtrer sur le Peer-to-Peer (niveau 1 ou 3) fonctionne

--- charger_listes.py
+++ charger_listes.py
@@ -1,13 +1,30 @@
-from urllib.request import urlopen
-chemin_bt_level_1 = r'http://list.iblocklist.com/?list=bt_level1&fileformat=p2p&archiveformat=gz'
+from io import BytesIO
+import gzip
+from urllib.request import Request
+from urllib.request import urlopen 
 
+chemin_bt_level_1 = r'http://list.iblocklist.com/?list=bt_level1&fileformat=p2p&archiveformat=gz'
+chemin_bt_level_3 = r'http://list.iblocklist.com/?list=bt_level3&fileformat=p2p&archiveformat=gz'
 
 def charger(chemin):
-	with urlopen(chemin) as f:
-		print(f.get('Content-Encoding'))
-		#print(f.read())
+
+	request = Request(chemin)
+	request.add_header('Accept-encoding', 'gzip')
+	response = urlopen(request)
+	print(response.info())
+	print('Content-Encoding', response.info().get('Content-Encoding') )
+	info = response.info()
+	if True or info.get('Content-Encoding') == 'gzip' or info.get('Content-Type') == 'application/x-gzip':		
+		print('if')
+		buf = BytesIO( response.read())
+		f = gzip.GzipFile(fileobj=buf)
+		data = f.read()
+		print(data)
+	else:
+		print('else')
+		print(response.read())
 	pass;
 
 
 if __name__ == '__main__':
-	charger(chemin_bt_level_1)
+	charger(chemin_bt_level_3)
