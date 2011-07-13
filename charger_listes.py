beedaada514040303e6992e140b1007d23755b19
Axel de Sablet <contact@axel-desablet.fr>
2011-07-14 00:11:57 +0200

    Marche avec les trois fichiers gzippes (en local). Produit un fichier de sortie de 17 Mo.

--- charger_listes.py
+++ charger_listes.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-import gzip
+import gzip, codecs
 from urllib.request import Request
 from urllib.request import urlopen 
 
@@ -12,13 +12,27 @@ def charger(chemin):
 	request.add_header('Accept-encoding', 'gzip')
 	response = urlopen(request)
 	print(response.info())
-	print('Content-Encoding', response.info().get('Content-Encoding') )
 	info = response.info()
 	buf = BytesIO( response.read())
 	f = gzip.GzipFile(fileobj=buf)
 	data = f.read()
 	return data.decode(errors='ignore')
 
+def main():
+	contenus = []
+#	adresses = [chemin_bt_level_1, chemin_bt_level_2, chemin_bt_level_3]
+	adresses = [
+'file:/Users/axel/Downloads/bt_level1.gz', 
+'file:/Users/axel/Downloads/bt_level2.gz', 
+'file:/Users/axel/Downloads/bt_level3.txt']
+
+	for adresse in adresses:
+		contenus.append(charger(adresse))
+
+	with codecs.open('./liste_filtrage_bittorrent.txt', 'w', encoding='utf8', errors='ignore') as sortie:
+		for contenu in contenus:
+			sortie.write(contenu)
+
 
 if __name__ == '__main__':
-	print (charger(r'file:/Users/axel/Downloads/bt_level3.txt'))
+	main()
